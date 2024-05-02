298a5916b7644f84a4065776ecf30da1229095e9
Bernardodass <bernardo113322@gmail.com>
2024-05-02 15:19:49 -0300

    contato

---
+++ js/contato.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* selecionando os elementos que serão manupulados */
+const formulario = document.querySelector("form");
+const campocep = document.querySelector("#cep");
+const campoendereco = document.querySelector("#endereco");
+const campobairro = document.querySelector("#bairro");
+const campocidade = document.querySelector("#cidade");
+const campoestado = document.querySelector("#estado");
+const botaobuscar = document.querySelector("#buscar");
+const mensagem = document.querySelector("#status");
