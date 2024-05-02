6cf8b19d9b29b31ea05280feac30d1fdcfaa2b47
Bernardodass <bernardo113322@gmail.com>
2024-05-02 15:27:48 -0300

    form

--- js/contato.js
+++ js/contato.js
@@ -1,11 +1,18 @@
 'use strict';
 
-/* selecionando os elementos que serão manupulados */
+// selecionando os elementos que serão manupulados
 const formulario = document.querySelector("form");
-const campocep = document.querySelector("#cep");
-const campoendereco = document.querySelector("#endereco");
-const campobairro = document.querySelector("#bairro");
-const campocidade = document.querySelector("#cidade");
-const campoestado = document.querySelector("#estado");
-const botaobuscar = document.querySelector("#buscar");
-const mensagem = document.querySelector("#status");
+const campocep = formulario.querySelector("#cep");
+const campoendereco = formulario.querySelector("#endereco");
+const campobairro = formulario.querySelector("#bairro");
+const campocidade = formulario.querySelector("#cidade");
+const campoestado = formulario.querySelector("#estado");
+const botaobuscar = formulario.querySelector("#buscar");
+const mensagem = formulario.querySelector("#status");
+
+// detectando quando o botão de buscar CEP é acionado
+botaobuscar.addEventListener("click", function(event){
+    // anular o comportamento padrão de rediricionamento/recarregamento da página. Sempre acontece ao trabalhar com <a> e <form>.
+    event.preventDefault();
+    
+});
